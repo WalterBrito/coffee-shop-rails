4a9305c7e82dbcc9df79ee29df6f92faed8a3198
WalterBrito <waltter.britto@gmail.com>
2017-04-05 22:41:12 -0300

    add css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
   resources :coffee
+  resources :donuts
+  resources :cake
+  resources :tea
+  resources :about
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
